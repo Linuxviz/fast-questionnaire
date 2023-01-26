c70eb0b22153fa8d94acf3793e030b494f05aad9
Linuxviz <Linuxviz@yandex.ru>
2023-01-26 18:19:54 +0700

    mongodb work

--- app/app.py
+++ app/app.py
@@ -1,15 +1,52 @@
-from typing import Union
+from typing import Union, Optional, List
 
+from beanie import init_beanie, Document, Link
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
+"""
+DB
+"""
 
-@app.get("/")
-def read_root():
+
+# class QuestionSet(Document):
+
+class User(Document):  # This is the model
+    user_name: str
+    name: Optional[str] = None
+    middle_name: Optional[str] = None
+    last_name: Optional[str] = None
+    password: str
+    is_active: bool
+    # questions: List[Link[QuestionSet]]
+
+
+async def db_init():
+    # Create Motor client
+    client = AsyncIOMotorClient("mongodb://database:27017/mongodb")
+    # Init beanie with the Product document class
+    await init_beanie(database=client.db_name, document_models=[User])
+
+
+"""
+ENDDB
+"""
+
+
+@app.on_event("startup")
+async def startup_event():
+    await db_init()
+    print("complete")
+
+
+@app.post("/")
+async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/a")
+async def read_item(item: User):
+    new_user = await User(**item.dict()).insert()
+    return new_user
