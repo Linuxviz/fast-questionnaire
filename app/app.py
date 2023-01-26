d7aa3edd591da72ad12612ff4c9094ff29788514
Linuxviz <Linuxviz@yandex.ru>
2023-01-26 22:50:49 +0700

    update views

--- app/app.py
+++ app/app.py
@@ -29,7 +29,7 @@ class QuestionSet(Document):
     description: str
     email_letter: Optional[str]
     questions: List[OpenQuestion]
-    callbacks: Dict[uuid.UUID, OpenCallback]  # key - question_id
+    callbacks: Dict[str, OpenCallback]  # key - question_id # key ==uuid4
     users: "List[Link[User]]"
 
 
@@ -50,7 +50,7 @@ async def db_init():
     # Create Motor client
     client = AsyncIOMotorClient("mongodb://database:27017/mongodb")
     # Init beanie with the Product document class
-    await init_beanie(database=client.db_name, document_models=[User])
+    await init_beanie(database=client.db_name, document_models=[User, QuestionSet])
 
 
 """
@@ -64,12 +64,23 @@ async def startup_event():
     print("complete")
 
 
+""" Views """
+
+
 @app.post("/")
 async def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/question_sets/{question_set_id}")
 async def read_item(question_set_id: PydanticObjectId) -> QuestionSet:
     question_set = await QuestionSet.get(question_set_id)
     return question_set
+
+
+@app.post("/question_sets/")
+async def read_item(question_set: QuestionSet) -> QuestionSet:
+    new_question_set = await QuestionSet(**question_set.dict()).save()
+    return new_question_set
+
+
+""" EndViews """
